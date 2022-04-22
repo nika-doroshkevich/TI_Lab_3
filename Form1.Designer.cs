233ab99f37180ab724deb6d5fb92d823ca01d7b7
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2022-04-22 22:27:19 +0300

    Files for TI for Lab_3

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace Lab_3_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pTextBox = new System.Windows.Forms.TextBox();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.kTextBox = new System.Windows.Forms.TextBox();
+            this.currDataTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.pLabel = new System.Windows.Forms.Label();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.kLabel = new System.Windows.Forms.Label();
+            this.currDataLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.chooseGLabel = new System.Windows.Forms.Label();
+            this.chooseGListBox = new System.Windows.Forms.ListBox();
+            this.countOfGTextBox = new System.Windows.Forms.TextBox();
+            this.countOfGLabel = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // pTextBox
+            // 
+            this.pTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pTextBox.Location = new System.Drawing.Point(77, 104);
+            this.pTextBox.Name = "pTextBox";
+            this.pTextBox.Size = new System.Drawing.Size(151, 35);
+            this.pTextBox.TabIndex = 0;
+            this.pTextBox.Leave += new System.EventHandler(this.pTextBox_Leave);
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xTextBox.Location = new System.Drawing.Point(281, 104);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(151, 35);
+            this.xTextBox.TabIndex = 1;
+            // 
+            // kTextBox
+            // 
+            this.kTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kTextBox.Location = new System.Drawing.Point(483, 104);
+            this.kTextBox.Name = "kTextBox";
+            this.kTextBox.Size = new System.Drawing.Size(151, 35);
+            this.kTextBox.TabIndex = 2;
+            // 
+            // currDataTextBox
+            // 
+            this.currDataTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.currDataTextBox.Location = new System.Drawing.Point(77, 346);
+            this.currDataTextBox.Name = "currDataTextBox";
+            this.currDataTextBox.Size = new System.Drawing.Size(930, 35);
+            this.currDataTextBox.TabIndex = 3;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultTextBox.Location = new System.Drawing.Point(77, 434);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(930, 35);
+            this.resultTextBox.TabIndex = 4;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encryptButton.Location = new System.Drawing.Point(77, 521);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(126, 60);
+            this.encryptButton.TabIndex = 5;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decryptButton.Location = new System.Drawing.Point(281, 521);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(126, 60);
+            this.decryptButton.TabIndex = 6;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chooseFileButton.Location = new System.Drawing.Point(492, 521);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(165, 60);
+            this.chooseFileButton.TabIndex = 7;
+            this.chooseFileButton.Text = "Choose File";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.chooseFileButton_Click);
+            // 
+            // pLabel
+            // 
+            this.pLabel.AutoSize = true;
+            this.pLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pLabel.Location = new System.Drawing.Point(72, 72);
+            this.pLabel.Name = "pLabel";
+            this.pLabel.Size = new System.Drawing.Size(96, 29);
+            this.pLabel.TabIndex = 8;
+            this.pLabel.Text = "Enter p:";
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xLabel.Location = new System.Drawing.Point(276, 72);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(93, 29);
+            this.xLabel.TabIndex = 9;
+            this.xLabel.Text = "Enter x:";
+            // 
+            // kLabel
+            // 
+            this.kLabel.AutoSize = true;
+            this.kLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kLabel.Location = new System.Drawing.Point(478, 72);
+            this.kLabel.Name = "kLabel";
+            this.kLabel.Size = new System.Drawing.Size(94, 29);
+            this.kLabel.TabIndex = 10;
+            this.kLabel.Text = "Enter k:";
+            // 
+            // currDataLabel
+            // 
+            this.currDataLabel.AutoSize = true;
+            this.currDataLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.currDataLabel.Location = new System.Drawing.Point(72, 314);
+            this.currDataLabel.Name = "currDataLabel";
+            this.currDataLabel.Size = new System.Drawing.Size(150, 29);
+            this.currDataLabel.TabIndex = 11;
+            this.currDataLabel.Text = "Current data:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(72, 402);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(87, 29);
+            this.resultLabel.TabIndex = 12;
+            this.resultLabel.Text = "Result:";
+            // 
+            // chooseGLabel
+            // 
+            this.chooseGLabel.AutoSize = true;
+            this.chooseGLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chooseGLabel.Location = new System.Drawing.Point(679, 72);
+            this.chooseGLabel.Name = "chooseGLabel";
+            this.chooseGLabel.Size = new System.Drawing.Size(123, 29);
+            this.chooseGLabel.TabIndex = 13;
+            this.chooseGLabel.Text = "Choose g:";
+            // 
+            // chooseGListBox
+            // 
+            this.chooseGListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chooseGListBox.FormattingEnabled = true;
+            this.chooseGListBox.ItemHeight = 29;
+            this.chooseGListBox.Location = new System.Drawing.Point(684, 104);
+            this.chooseGListBox.Name = "chooseGListBox";
+            this.chooseGListBox.Size = new System.Drawing.Size(185, 207);
+            this.chooseGListBox.TabIndex = 14;
+            this.chooseGListBox.SelectedIndexChanged += new System.EventHandler(this.chooseGListBox_SelectedIndexChanged);
+            // 
+            // countOfGTextBox
+            // 
+            this.countOfGTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.countOfGTextBox.Location = new System.Drawing.Point(917, 104);
+            this.countOfGTextBox.Name = "countOfGTextBox";
+            this.countOfGTextBox.ReadOnly = true;
+            this.countOfGTextBox.Size = new System.Drawing.Size(151, 35);
+            this.countOfGTextBox.TabIndex = 15;
+            // 
+            // countOfGLabel
+            // 
+            this.countOfGLabel.AutoSize = true;
+            this.countOfGLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.countOfGLabel.Location = new System.Drawing.Point(912, 72);
+            this.countOfGLabel.Name = "countOfGLabel";
+            this.countOfGLabel.Size = new System.Drawing.Size(128, 29);
+            this.countOfGLabel.TabIndex = 16;
+            this.countOfGLabel.Text = "Count of g:";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 680);
+            this.Controls.Add(this.countOfGLabel);
+            this.Controls.Add(this.countOfGTextBox);
+            this.Controls.Add(this.chooseGListBox);
+            this.Controls.Add(this.chooseGLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.currDataLabel);
+            this.Controls.Add(this.kLabel);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.pLabel);
+            this.Controls.Add(this.chooseFileButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.currDataTextBox);
+            this.Controls.Add(this.kTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.pTextBox);
+            this.Name = "Form1";
+            this.Text = "Lab_3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pTextBox;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox kTextBox;
+        private System.Windows.Forms.TextBox currDataTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button chooseFileButton;
+        private System.Windows.Forms.Label pLabel;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label kLabel;
+        private System.Windows.Forms.Label currDataLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label chooseGLabel;
+        private System.Windows.Forms.ListBox chooseGListBox;
+        private System.Windows.Forms.TextBox countOfGTextBox;
+        private System.Windows.Forms.Label countOfGLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab_3_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public static int g = 0, p = 0;
+        public static string fileName = "";
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            currDataTextBox.Text = "";
+            resultTextBox.Text = "";
+
+            string xStr = (xTextBox.Text).ToString();
+            string kStr = (kTextBox.Text).ToString();
+
+            if (xStr == "" || kStr == "")
+            {
+                MessageBox.Show("Please, enter data!");
+                return;
+            }
+
+            xStr = ToCorrectData(xStr);
+            kStr = ToCorrectData(kStr);
+
+            if (xStr == "" || kStr == "")
+            {
+                MessageBox.Show("Please, enter data consisting of digits!");
+                return;
+            }
+
+            int x = int.Parse(xStr);
+            int k = int.Parse(kStr);
+
+            if (!check(x, k))
+                return;
+
+            int y = powByMod(g, x, p);
+
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(fs);
+            fileName = fileName.Insert(fileName.LastIndexOf('.'), "New");
+            BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.OpenOrCreate));
+
+            byte b = 0;
+            byte a = (byte)(powByMod(g, k, p));
+            short b2 = 0;
+            short a2 = (short)(powByMod(g, k, p));
+            int input = 0;
+
+            while (reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                byte byteMessage = reader.ReadByte();
+                if (input++ < 200)
+                    currDataTextBox.Text += byteMessage.ToString() + " ";
+                if (p < 256)
+                {
+                    b = (byte)(powByMod(y, k, p) * byteMessage % p);
+                    if (input++ < 200)
+                        resultTextBox.Text += a.ToString() + "," + b.ToString() + "  ";
+                    writer.Write(a);
+                    writer.Write(b);
+                }
+                else if (p < 65536)
+                {
+                    b2 = (short)(powByMod(y, k, p) * byteMessage % p);
+                    if (input++ < 200)
+                        resultTextBox.Text += a2.ToString() + "," + b2.ToString() + "  ";
+                    writer.Write(a2);
+                    writer.Write(b2);
+                }
+            }
+            reader.Close();
+            writer.Close();
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            currDataTextBox.Text = "";
+            resultTextBox.Text = "";
+
+            string xStr = (xTextBox.Text).ToString();
+            string kStr = (kTextBox.Text).ToString();
+
+            if (xStr == "" || kStr == "")
+            {
+                MessageBox.Show("Please, enter data!");
+                return;
+            }
+
+            xStr = ToCorrectData(xStr);
+            kStr = ToCorrectData(kStr);
+
+            if (xStr == "" || kStr == "")
+            {
+                MessageBox.Show("Please, enter data consisting of digits!");
+                return;
+            }
+
+            int x = int.Parse(xStr);
+            int k = int.Parse(kStr);
+
+            if (!check(x, k))
+                return;
+
+            int y = powByMod(g, x, p);
+
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(fs);
+            fileName = fileName.Insert(fileName.LastIndexOf('.'), "New");
+            BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.OpenOrCreate));
+
+            byte b = 0;
+            byte a = (byte)(powByMod(g, k, p));
+            short b2 = 0;
+            short a2 = (short)(powByMod(g, k, p));
+            int input = 0;
+            
+            while (reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                if (p < 256)
+                {
+                    a = reader.ReadByte();
+                    b = reader.ReadByte();
+
+                    byte t = decrypt(a, b, p, x);
+                    if (input++ < 200)
+                        resultTextBox.Text += t.ToString() + " ";
+                    writer.Write(t);
+                }
+                else if (p < 65536)
+                {
+                    a2 = reader.ReadInt16();
+                    b2 = reader.ReadInt16();
+
+                    byte t = decrypt(a2, b2, p, x);
+                    if (input++ < 200)
+                        resultTextBox.Text += t.ToString() + " ";
+                    writer.Write(t);
+                } 
+            }
+            reader.Close();
+            writer.Close();
+        }
+
+        public static bool check(int x, int k)
+        {
+            if (p == 0)
+            {
+                MessageBox.Show("Please, enter p!");
+                return false;
+            }
+
+            if (!checkPrime(p))
+            {
+                MessageBox.Show("p is not prime!");
+                return false;
+            }
+
+            if (p <= 255)
+            {
+                MessageBox.Show("p <= 255!");
+                return false;
+            }
+
+            if (g == 0)
+            {
+                MessageBox.Show("Please, choose g!");
+                return false;
+            }
+
+            if (!((GCD(k, p - 1) == 1) && k > 1 && k < (p - 1)))
+            {
+                MessageBox.Show("k is not coprime with p or not 1<k<p-1!");
+                return false;
+            }
+
+            if(!(x > 1 && x < (p - 1)))
+            {
+                MessageBox.Show("x not 1<x<p-1!");
+                return false;
+            }
+
+            if (fileName == "")
+            {
+                MessageBox.Show("Please, choose file!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void chooseFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Dialog = new OpenFileDialog();
+            if (Dialog.ShowDialog() == DialogResult.OK)
+                fileName = Dialog.FileName;
+        }
+
+        private static string ToCorrectData(string str)
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+                if (str[i] >= '0' && str[i] <= '9')
+                    result += str[i];
+
+            return result;
+        }
+
+        private void pTextBox_Leave(object sender, EventArgs e)
+        {
+            string pStr = (pTextBox.Text).ToString();
+            if (pStr == "")
+            {
+                MessageBox.Show("Please, enter p!");
+                chooseGListBox.Items.Clear();
+                countOfGTextBox.Text = "";
+                return;
+            }
+
+            pStr = ToCorrectData(pStr);
+            if (pStr == "")
+            {
+                MessageBox.Show("Please, enter p consisting of digits!");
+                return;
+            }
+
+            p = int.Parse(pStr);
+
+            if (!checkPrime(p))
+            {
+                MessageBox.Show("p is not prime!");
+                return;
+            }
+
+            if (chooseGListBox.Items.Count != 0)
+                chooseGListBox.Items.Clear();
+
+            int count = 0;
+            foreach (int i in findAntid(p))
+            {
+                chooseGListBox.Items.Add(i.ToString());
+                count++;
+            }
+
+            countOfGTextBox.Text = count.ToString();
+        }
+
+        private void chooseGListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            g = int.Parse(chooseGListBox.SelectedItem.ToString());
+        }
+
+        public static List<int> findAntid(int p)
+        {
+            HashSet<int> prime = findPrimeDiv(p - 1);
+            List<int> antid = new List<int>();
+            for (int x = 2; x < p; x++)
+            {
+                bool f = true;
+                foreach (int s in prime)
+                    f = f && powByMod(x, (p - 1) / s, p) != 1;
+                if (f)
+                    antid.Add(x);
+            }
+
+            return antid;
+        }
+
+        public static HashSet<int> findPrimeDiv(int num)
+        {
+            if (num == 1)
+                return new HashSet<int>(1);
+            int i;
+            for (i = 2; num % i != 0; i++) ;
+            HashSet<int> hs = new HashSet<int>();
+            hs.Add(i);
+            foreach (int x in findPrimeDiv(num / i))
+                hs.Add(x);
+
+            return hs;
+        }
+
+        public static bool checkPrime(int number)
+        {
+            if (number == 2)
+                return true;
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int randomNumber = r.Next(2, number);
+                if (GCD(randomNumber, number) != 1)
+                    return false;
+                if (powByMod(randomNumber, number - 1, number) != 1)
+                    return false;
+            }
+            return true;
+        }
+
+        public static int GCD(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+
+        public static int powByMod(int a, int b, int m)
+        {
+            if (b == 0)
+                return 1;
+            if (b % 2 == 0)
+            {
+                int t = powByMod(a, b / 2, m);
+                return t * t % m;
+            }
+
+            return a * powByMod(a, b - 1, m) % m;
+        }
+
+        public static byte decrypt(int a, int b, int p, int x)
+        {
+            int inversA = powByMod(a, p - 2, p);
+            int temp = powByMod(inversA, x, p);
+            byte m = (byte)(b * temp % p);
+            return m;
+        }
+        
+    }
+}
